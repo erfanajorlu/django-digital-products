28f1d92fbdb8dc682d577285ebb157f98f15c054
erfan <erfanajoglu098@gmail.com>
2023-09-01 21:53:00 +0330

    Product class created

--- products/models.py
+++ products/models.py
@@ -10,3 +10,12 @@ class Category(models.Model):
     is_enable = models.BooleanField(_('is_enable') , default=True)
     created_time = models.DateTimeField(_('created_time') , auto_now_add=True)
     updated_time = models.DateTimeField(_('updated_time') , auto_now=True)
+
+class Product(models.Model):
+    categories = models.ManyToManyField('Category')
+    title = models.CharField(_('title') , max_length=50)
+    description = models.TextField(_('description') , blank=True)
+    avatar = models.ImageField(_('avatar') , upload_to='categories')
+    is_enable = models.BooleanField(_('is_enable') , default=True)
+    created_time = models.DateTimeField(_('created_time') , auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated_time') , auto_now=True)
