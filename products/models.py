2570a2f35adc907af2ecd61499521e93194ccc1b
erfan <erfanajoglu098@gmail.com>
2023-09-02 15:23:00 +0330

    meta class add

--- products/models.py
+++ products/models.py
@@ -11,6 +11,11 @@ class Category(models.Model):
     created_time = models.DateTimeField(_('created_time') , auto_now_add=True)
     updated_time = models.DateTimeField(_('updated_time') , auto_now=True)
 
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural =   _('Categories')
+
 class Product(models.Model):
     categories = models.ManyToManyField('Category' , verbose_name=_('categories') ,blank=True)
     title = models.CharField(_('title') , max_length=50)
@@ -20,6 +25,11 @@ class Product(models.Model):
     created_time = models.DateTimeField(_('created time') , auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time') , auto_now=True)
 
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('Product')
+        verbose_name_plural =   _('Products')
+
 class File(models.Model):
     product = models.ForeignKey('Product' ,verbose_name=_('product') , on_delete=models.CASCADE)
     title = models.CharField(_('title') , max_length=50)
@@ -27,3 +37,8 @@ class File(models.Model):
     is_enable = models.BooleanField(_('is enable') , default=True)
     created_time = models.DateTimeField(_('created time') , auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time') , auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
