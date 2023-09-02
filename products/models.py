db6ce15f61a8bb8ec1338b6d3203e76fdd1fec14
erfan <erfanajoglu098@gmail.com>
2023-09-02 15:18:13 +0330

    file class created

--- products/models.py
+++ products/models.py
@@ -12,10 +12,18 @@ class Category(models.Model):
     updated_time = models.DateTimeField(_('updated_time') , auto_now=True)
 
 class Product(models.Model):
-    categories = models.ManyToManyField('Category')
+    categories = models.ManyToManyField('Category' , verbose_name=_('categories') ,blank=True)
     title = models.CharField(_('title') , max_length=50)
     description = models.TextField(_('description') , blank=True)
-    avatar = models.ImageField(_('avatar') , upload_to='categories')
-    is_enable = models.BooleanField(_('is_enable') , default=True)
-    created_time = models.DateTimeField(_('created_time') , auto_now_add=True)
-    updated_time = models.DateTimeField(_('updated_time') , auto_now=True)
+    avatar = models.ImageField(_('avatar') , upload_to='products/') 
+    is_enable = models.BooleanField(_('is enable') , default=True)
+    created_time = models.DateTimeField(_('created time') , auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time') , auto_now=True)
+
+class File(models.Model):
+    product = models.ForeignKey('Product' ,verbose_name=_('product') , on_delete=models.CASCADE)
+    title = models.CharField(_('title') , max_length=50)
+    file  = models.FileField(_('file') ,upload_to=('files/%Y/%m/%d/'))
+    is_enable = models.BooleanField(_('is enable') , default=True)
+    created_time = models.DateTimeField(_('created time') , auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time') , auto_now=True)
